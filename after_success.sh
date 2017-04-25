861de1a122c13297f19bbf70a0ff9bf8ccbf78a0
BaardBouvet <baard.johansen@bouvet.no>
2017-04-25 20:13:47 +0200

    Create after_success.sh

---
+++ after_success.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## Deploys the master branch of a sesam-community project to dockerhub.
+SLUG="$TRAVIS_REPO_SLUG"
+REPO_NAME=${SLUG#sesam-community/}
+if [ "$TRAVIS_BRANCH" == "master" ];
+then
+  docker build -t sesamcommunity/$REPO_NAME .;
+  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+  docker push sesamcommunity/$REPO_NAME:latest;
+fi
