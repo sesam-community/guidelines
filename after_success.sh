a621ffa516b359b58c0dcb3bb6027fc2a9c0a5f4
BaardBouvet <baard.johansen@bouvet.no>
2017-04-25 21:53:21 +0200

    Update after_success.sh

--- after_success.sh
+++ after_success.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ## Deploys the master branch of a sesam-community project to dockerhub.
+# make the build fail on any commands
+set -e
 SLUG="$TRAVIS_REPO_SLUG"
 REPO_NAME=${SLUG#sesam-community/}
-if [ "$TRAVIS_BRANCH" == "master" ];
+if [ "$TRAVIS_BRANCH" == "master" ]
 then
-  docker build -t sesamcommunity/$REPO_NAME .;
-  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-  docker push sesamcommunity/$REPO_NAME:latest;
+  docker build -t sesamcommunity/$REPO_NAME .
+  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+  docker push sesamcommunity/$REPO_NAME:latest
 fi
