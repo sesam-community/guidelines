5684e98807ba775539f783c5465b74b34569d0d6
Anders Borud <post@andersborud.com>
2019-10-04 10:21:05 +0200

    Change so only releases/tags trigger image builds
    Latest tag will now be set in addition to version tags everytime a
    release gets built.

--- after_success.sh
+++ after_success.sh
@@ -10,17 +10,14 @@ GIT_REPO_OWNER="${SLUG/%\/*/}"
 #add similar line as below for each contributer that has GIT_REPO_OWVER and DOCKER_REPO_OWNER different
 DOCKER_REPO_OWNER=${GIT_REPO_OWNER/sesam-community/sesamcommunity}
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]
-then
-  DOCKER_REPO_TAG="latest"
-elif [ -n "$TRAVIS_TAG" ]
+if [ -n "$TRAVIS_TAG" ]
 then
   DOCKER_REPO_TAG="$TRAVIS_TAG"
 fi
 
 if [ -n "$DOCKER_REPO_TAG" ]
 then
-  docker build --label Commit="$TRAVIS_COMMIT" --label BuildNumber="$TRAVIS_BUILD_NUMBER" --label RepoSlug="$TRAVIS_REPO_SLUG" -t $DOCKER_REPO_OWNER/$REPO_NAME:$DOCKER_REPO_TAG .
+  docker build --label Commit="$TRAVIS_COMMIT" --label BuildNumber="$TRAVIS_BUILD_NUMBER" --label RepoSlug="$TRAVIS_REPO_SLUG" -t $DOCKER_REPO_OWNER/$REPO_NAME:$DOCKER_REPO_TAG -t $DOCKER_REPO_OWNER/$REPO_NAME:latest .
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
   docker push $DOCKER_REPO_OWNER/$REPO_NAME:$DOCKER_REPO_TAG
 fi
