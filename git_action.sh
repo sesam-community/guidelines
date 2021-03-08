f7ced1ac5ed483576ab4953deca176ebbfe6a6a9
Gabriell Constantin Vig <46919496+gabriellsesam@users.noreply.github.com>
2021-03-08 11:50:00 +0100

    Update git_action.sh

--- git_action.sh
+++ git_action.sh
@@ -15,16 +15,16 @@ if  [ "${GITHUB_REF:0:10}" = "refs/tags/" ]
 then
   _DOCKER_REPO_TAG="${GITHUB_REF:10}"
 fi
-echo "Docker image is set to'${DOCKER_USERNAME}/${_REPO_NAME}:${_DOCKER_REPO_TAG}'"
+echo "Docker image is set to'${secrets.DOCKER_USERNAME}/${_REPO_NAME}:${_DOCKER_REPO_TAG}'"
 
 
 #build docker image
-docker build --label Commit="${GITHUB_SHA}" --label BuildNumber="${GITHUB_RUN_NUMBER}" --label RepoSlug="${_REPO}" -t ${DOCKER_USERNAME}/${_REPO_NAME}:${_DOCKER_REPO_TAG} .
+docker build --label Commit="${GITHUB_SHA}" --label BuildNumber="${GITHUB_RUN_NUMBER}" --label RepoSlug="${_REPO}" -t ${secrets.DOCKER_USERNAME}/${_REPO_NAME}:${_DOCKER_REPO_TAG} .
 
 #push to dockerhub if tagged or pushed to master
 if  [ "${GITHUB_REF:0:10}" = "refs/tags/" ] || [ "${GITHUB_REF}" = "refs/heads/master" ]
 then
-  docker push ${DOCKER_USERNAME}/${_REPO_NAME}:${_DOCKER_REPO_TAG}
+  docker push ${secrets.DOCKER_USERNAME}/${_REPO_NAME}:${_DOCKER_REPO_TAG}
   docker logout
 else
   echo "Skipping docker-push as per the logic"
