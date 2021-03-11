43ee8205209cdbdf19e590c5519cd41f5d955c68
Melih Egemen Yavuz <egemen.yavuz@soprasteria.com>
2021-03-11 10:18:16 +0100

    Update git_action.sh

--- git_action.sh
+++ git_action.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ## Deploys the master branch and any tags of a sesam-community or a contributer project to dockerhub.
-## Requires following ENVVARS: GITHUB_REPO, GITHUB_BASE_REF, GITHUB_REF, GITHUB_SHA, GITHUB_RUN_NUMBER, DOCKER_USERNAME, DOCKER_PASSWORD
+## Requires following ENVVARS: GITHUB_REPO, GITHUB_BASE_REF, GITHUB_REF, GITHUB_SHA, GITHUB_RUN_NUMBER, DOCKER_USERNAME
+## Supports  optional ENVVARS: DOCKER_REPO_NAME, DOCKER_ORGNAME
 
 # make the build fail on any commands
 set -v
@@ -9,13 +10,14 @@ set -e
 _REPO="${GITHUB_REPO}"
 _DEFAULT_REPO_NAME="${_REPO/#*\//}"
 _REPO_NAME="${DOCKER_REPO_NAME:-$_DEFAULT_REPO_NAME}"
+_DOCKER_ID="${DOCKER_ORGNAME:-$DOCKER_USERNAME}"
 
 _DOCKER_REPO_TAG="development"
 if  [ "${GITHUB_REF:0:10}" = "refs/tags/" ]
 then
   _DOCKER_REPO_TAG="${GITHUB_REF:10}"
 fi
-echo "Docker image is set to'${DOCKER_USERNAME}/${_REPO_NAME}:${_DOCKER_REPO_TAG}'"
+echo "Docker image is set to'${_DOCKER_ID}/${_REPO_NAME}:${_DOCKER_REPO_TAG}'"
 
 
 #build docker image
@@ -24,13 +26,13 @@ docker build --label Commit="${GITHUB_SHA}" --label BuildNumber="${GITHUB_RUN_NU
 #push to dockerhub if tagged or pushed to master
 if  [ "${GITHUB_REF:0:10}" = "refs/tags/" ] || [ "${GITHUB_REF}" = "refs/heads/master" ]
 then
-  if [ -z "${DOCKER_USERNAME}" ]
+  if [ -z "${_DOCKER_ID}" ]
   then
-    echo "DOCKER_USERNAME is not set.Cannot push to docker registry"
+    echo "Neither DOCKER_ORGNAME nor DOCKER_USERNAME is set.Cannot push to docker registry!"
     exit 1
   fi
-  docker tag ${_REPO_NAME}:${_DOCKER_REPO_TAG} ${DOCKER_USERNAME}/${_REPO_NAME}:${_DOCKER_REPO_TAG}
-  docker push ${DOCKER_USERNAME}/${_REPO_NAME}:${_DOCKER_REPO_TAG}
+  docker tag ${_REPO_NAME}:${_DOCKER_REPO_TAG} ${_DOCKER_ID}/${_REPO_NAME}:${_DOCKER_REPO_TAG}
+  docker push ${_DOCKER_ID}/${_REPO_NAME}:${_DOCKER_REPO_TAG}
   docker logout
 else
   echo "Skipping docker-push as per the logic"
